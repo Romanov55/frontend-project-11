c429a8421b857e658d955355e88f439b77898cc5
Romanov55 <Romanov20let@gmail.com>
2023-03-16 16:11:52 +0600

    up app

--- src/app.js
+++ src/app.js
@@ -1,92 +1,130 @@
-import * as yup from 'yup';
-import onChange from 'on-change';
-import view from './view';
-import axios from 'axios';
-import parser from './parser'
 import i18next from 'i18next';
-import ru from './local/ru';
+import _ from 'lodash';
+import axios from 'axios';
+import * as yup from 'yup';
+import ru from './locales/ru.js';
+import view from './view.js';
+import parse from './parser.js';
 
-// валидация введённых значений
-// const validate = (url, state) => {
-//   const schema = yup.object().shape({
-//     url: yup.string().url('invalidURL').notOneOf(state.data.feeds
-//       .map((feed) => feed.url), 'alreadyExists').required('emptyField'),
-//   });
-//   return schema.validate({ url });
-// };
+const timeOut = 5000;
 
-// const proxify = (url) => {
-//   const urlWithProxy = new URL('/get', 'https://allorigins.hexlet.app');
-//   urlWithProxy.searchParams.set('url', url);
-//   urlWithProxy.searchParams.set('disableCache', 'true');
-//   return urlWithProxy.toString();
-// };
+const setIds = (data) => {
+  const feedId = _.uniqueId();
+  const { title, description } = data.feed;
+  const feed = { feedId, title, description };
+  const posts = data.posts.map((post) => ({ feedId, id: _.uniqueId(), ...post }));
+  return { feed, posts };
+}
 
-// const handleSubmitButtonEvent = (state, elements) => {
-//   const { data, formState } = state;
-//   const formData = new FormData(elements.form);
-//   const inputPath = formData.get('url');
-//   formState.status = 'valid';
-//   formState.processState = 'processing';
-//   validate(inputPath, state)
-//     .then(({ url }) => axios.get(proxify(url)))
-//     .then((response) => {
-//       const { contents } = response.data;
-//       const parsedData = parse(contents);
-//       formState.status = 'success';
-//       formState.processState = 'finished';
-//       parsedData.feed.url = inputPath;
-//       const { feed, posts } = normalizeData(parsedData);
-//       data.feeds = [...data.feeds, feed];
-//       data.posts = [...data.posts, ...posts];
-//     })
-//     .catch((err) => {
-//       formState.status = 'invalid';
-//       const currentError = (err.name === 'AxiosError') ? 'badNetwork' : err.message;
-//       formState.processError = currentError;
-//       formState.processState = 'failed';
-//     })
-//     .finally(() => {
-//       formState.processState = 'filling';
-//     });
-// };
+const generateURL = (link) => {
+  const url = new URL('https://allorigins.hexlet.app/get');
+  url.searchParams.append('disableCache', 'true');
+  url.searchParams.append('url', link);
+  return url;
+};
+
+const getFeedsPosts = (link) => axios.get(generateURL(link))
+  .then((response) => {
+    const parsedData = parse(response.data.contents);
+    return setIds(parsedData);
+  });
+
+const handleError = (error) => {
+  if (error.isParsingError) {
+    return 'rssError';
+  }
+  if (axios.isAxiosError(error)) {
+    return 'networkError';
+  }
+  return error.message.key ?? 'unknown';
+};
 
 export default () => {
-  const elements = {
-    form: document.querySelector('.rss-form'),
-    inputUrl: document.querySelector('#url-input'),
-    feedback: document.querySelector('.feedback'),
-  };
-
-  const initialState = {
-    data: {
-      feeds: [],
-      posts: [],
+  yup.setLocale({
+    mixed: {
+      notOneOf: () => ({ key: 'duplicate' }),
+      required: () => ({ key: 'required' }),
     },
-    form: {
-      status: 'valid',
-      processState: 'filling',
-      processError: '',
+    string: {
+      url: () => ({ key: 'invalidUrl' }),
     },
-  };
-
-  const i18n = i18next.createInstance();
-  i18n.init({
-    lng: ru,
-    debug: false,
-    resources: {
-      ru,
-    },
-  })
-
-  const state = onChange(initialState, view(elements, initialState, i18n))
-
-  console.log(state)
-
-
-  elements.form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    // handleSubmitButtonEvent(state, elements);
   });
+
+  const intialState = {
+    status: 'intial',
+    error: null,
+    feeds: [],
+    posts: [],
+    readPostsIds: new Set(),
+    modalPost: {
+      postId: '',
+    },
+  };
+
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    resources: { ru },
+  })
+    .then(() => {
+      const state = view(intialState, i18nextInstance);
+      const makeSchema = (validatedLinks) => yup.string()
+        .required()
+        .url()
+        .notOneOf(validatedLinks);
+
+      const form = document.querySelector('form.rss-form');
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const links = state.feeds.map((feed) => feed.link);
+        const schema = makeSchema(links);
+        const formData = new FormData(e.target);
+        const inputURL = formData.get('url').trim();
+        schema.validate(inputURL)
+          .then(() => {
+            state.status = 'processing';
+            state.error = null;
+            return getFeedsPosts(inputURL);
+          })
+          .then((normalizedData) => {
+            state.feeds.unshift(normalizedData.feed);
+            state.feeds[0].link = inputURL;
+            state.posts.unshift(...normalizedData.posts);
+            state.status = 'finished';
+          })
+          .catch((error) => {
+            state.error = handleError(error);
+            state.status = 'failed';
+          });
+      });
+
+      const postsContainer = document.querySelector('.posts');
+      postsContainer.addEventListener('click', (e) => {
+        const { id } = e.target.dataset;
+        if (!id) return;
+        state.readPostsIds.add(id);
+        state.modalPost.postId = id;
+        state.status = 'finished';
+      });
+
+      const checkForNewPosts = () => {
+        const promises = state.feeds
+          .map((feed, index) => getFeedsPosts(feed.link)
+            .then((response) => {
+              const { feedId } = state.feeds[index];
+              const filteredPosts = state.posts.filter((post) => post.feedId === feedId);
+              const currentNewPosts = _.differenceBy(response.posts, filteredPosts, 'title')
+                .map((post) => ({ feedId, id: _.uniqueId, ...post }));
+              if (currentNewPosts.length > 0) {
+                state.posts.unshift(...currentNewPosts);
+                state.status = 'finished';
+              }
+            })
+            .catch((err) => {
+              console.log(new Error(err.message));
+            }));
+        Promise.all(promises).finally(() => setTimeout(checkForNewPosts, timeOut));
+      };
+      checkForNewPosts();
+    });
 };
